e70376b4688feabe46b551da7b693b7c8d00a6b6
Drew Bowling <andrewwbowling@gmail.com>
2023-02-28 15:28:26 -0700

    created console log statements

--- variables.js
+++ variables.js
@@ -2,6 +2,9 @@ var email = "test@gmail.com"
 var greeting = "Hello World"
 var zipCode = "11111"
 
+email = "bobjohnson@gmail.com"
+greeting = "poop"
+zipCode = "11112"
 
 var levelCap = "60"
 var myAge = "31"
@@ -11,3 +14,8 @@ var favoriteColorPink = "true"
 var blueSky = "true"
 var flatEarth = "false"
 
+console.log (email)
+
+console.log (greeting)
+
+console.log (zipCode)
